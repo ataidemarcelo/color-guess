64d75f5b5dae7e3cde673184d0b1e8e50fa20602
Marcelo Ataíde <msa.trybe@gmail.com>
2022-04-25 09:05:26 -0300

    Requisito 4 - Adicione cores nas bolas elas devem ser geradas dinâmicamente

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 console.log('Project Color Guess!!!');
+
+function generateColor() {
+  const boxBalls = document.getElementById('box-balls');
+
+  for (let index = 1; index <= 6; index += 1) {
+    const divBall = document.createElement('div');
+    divBall.className = 'ball';
+    divBall.style.backgroundColor = 'black';
+
+    const red = Math.floor(Math.random() * 255);
+    const green = Math.floor(Math.random() * 255);
+    const blue = Math.floor(Math.random() * 255);
+
+    divBall.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+    boxBalls.appendChild(divBall);
+  }
+}
+
+generateColor();
