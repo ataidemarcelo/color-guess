56d8e761b62b5519d8d37d6338eecc050b0b7748
Marcelo Ataíde <msa.trybe@gmail.com>
2022-04-26 09:46:26 -0300

    Requisito 7 - Crie um placar que incremente 3 pontos para cada acerto no jogo

--- script.js
+++ script.js
@@ -27,7 +27,6 @@ function generateColors() {
   for (let index = 1; index <= 6; index += 1) {
     const divBall = document.createElement('div');
     divBall.className = 'ball';
-    divBall.style.backgroundColor = 'black';
 
     const rgb = generateRgb();
 
@@ -41,15 +40,29 @@ function generateColors() {
 }
 
 const answer = document.getElementById('answer');
+const scoreElement = document.getElementById('score');
+let score = 0;
+
+function win(item, colorGuess) {
+  const ele = item;
+  ele.addEventListener('click', (event) => {
+    if (colorGuess === event.target.style.backgroundColor) {
+      ele.style.border = '5px solid blue';
+      answer.innerText = 'Acertou!';
+      score += 3;
+      scoreElement.innerText = `${score}`;
+    } else {
+      ele.style.border = '5px solid red';
+      answer.innerText = 'Errou! Tente novamente!';
+      score -= 1;
+      scoreElement.innerText = `${score}`;
+    }
+  });
+}
+
 function checkWin(collection, colorGuess) {
   for (let index = 0; index < collection.length; index += 1) {
-    collection[index].addEventListener('click', (event) => {
-      if (colorGuess === event.target.style.backgroundColor) {
-        answer.innerText = 'Acertou!';
-      } else {
-        answer.innerText = 'Errou! Tente novamente!';
-      }
-    });
+    win(collection[index], colorGuess);
   }
 }
 
@@ -71,6 +84,7 @@ function resetGame() {
   answer.innerText = 'Escolha uma cor';
   deleteColors();
   generateColors();
+  checkWin(balls, color);
 }
 
 const btnResetGame = document.getElementById('reset-game');
